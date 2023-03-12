1c6764c3a58825132eb15c00db83fcd2bc8c134d
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2023-03-12 12:59:11 +0100

    hot fix

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Unittest module for the Amenity Class."""
+
+import unittest
+from datetime import datetime
+from models.amenity import Amenity
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestAmenity(unittest.TestCase):
+
+    """Test Cases for the Amenity class."""
+
+	def test_name(self)
+	self.name = Kelvin
+
+if __name__ == "__main__":
+    unittest.main()
